cee6b7f5465ba9597f12b01b1991cfdf70bcf563
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-22 13:28:47 +0800

    Updated test.

--- sepby-test.js
+++ sepby-test.js
@@ -2,10 +2,8 @@
 const assert = require( "assert" );
 const sepby = require( "./sepby.js" );
 
-assert.deepEqual( sepby( [ "hello", "world", 1, 2, 3, true, false, 5, 6 ],
-NUMBER ), [ 1, 2, 3, 5, 6, "hello", "world", true, false ], "should be equal" );
+assert.deepEqual( sepby( [ "hello", "world", 1, 2, 3, true, false, 5, 6 ], NUMBER ), [ 1, 2, 3, 5, 6, "hello", "world", true, false ], "should be equal" );
 
-assert.deepEqual( sepby( [ "hello", "world", 1, true, 6 ], UNDEFINED ),
-[ "hello", "world", 1, true, 6 ], "should be equal" );
+assert.deepEqual( sepby( [ "hello", "world", 1, true, 6 ], UNDEFINED ), [ "hello", "world", 1, true, 6 ], "should be equal" );
 
 console.log( "ok" );
