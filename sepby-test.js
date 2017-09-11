d491f862431fcd048ae4e101111dc8dd4587e234
Vinse Vinalon <vinsevinalon@gmail.com>
2017-09-12 01:59:02 +0800

    Update client/server unit test.

--- sepby-test.js
+++
@@ -1,9 +0,0 @@
-
-const assert = require( "assert" );
-const sepby = require( "./sepby.js" );
-
-assert.deepEqual( sepby( [ "hello", "world", 1, 2, 3, true, false, 5, 6 ], NUMBER ),
-	[ 1, 2, 3, 5, 6, "hello", "world", true, false ],
-	"should be equal to [ 1, 2, 3, 5, 6, 'hello', 'world', true, false ]" );
-
-console.log( "ok" );
